01f9843f336ba0af4d54d30fce3bab53651baa33
sherlockGH-coder <3122003350@mail2.gdut.edu.cn>
2024-12-25 16:00:36 +0800

    修改了情况说明的判断逻辑，删除了一些不必要的记录

--- data_preprocess.py
+++ data_preprocess.py
@@ -94,57 +94,214 @@ def process_course_data(data):
     # 再按任课教师维度更新情况说明
     data.loc[data['任课教师'].map(lambda x: len(data[data['任课教师'] == x])) == 1, '情况说明'] = '必选'
     
-    # 找出所有"任选其一"的课程组
-    optional_courses = data[data['情况说明'] == '任选其一']
+
+    # 处理有老师只教授一门课，但是却有多条记录的情况
+    # 找出每个教师的所有课程记录
+    teacher_courses = data.groupby('任课教师').agg({
+        '课程名称': lambda x: set(x),
+        '课程类型': lambda x: set(x)
+    }).reset_index()
+
+    # 找出所有记录课程名称和类型都相同的教师
+    same_course_teachers = teacher_courses[
+        (teacher_courses['课程名称'].map(len) == 1) & 
+        (teacher_courses['课程类型'].map(len) == 1)
+    ]['任课教师']
+
+    # 更新这些教师的情况说明
+    for teacher in same_course_teachers:
+        mask = data['任课教师'] == teacher
+        if data.loc[mask, '情况说明'].eq('任选其一').all():
+            data.loc[mask, '情况说明'] = '在该老师这门课程中任选其一'
+
+    # 处理所有课程都是任选其一的老师
+    # 找出所有课程都是"任选其一"的教师
+    all_optional_teachers = data.groupby('任课教师').filter(
+        lambda x: x['情况说明'].eq('任选其一').all()
+    )['任课教师'].unique()
+
+    potential_deletions = []
+    teachers_to_update = []
+
+    for teacher in all_optional_teachers:
+        teacher_courses = data[data['任课教师'] == teacher]
+        
+        # 如果教师有不同的课程
+        if len(teacher_courses[['课程名称', '课程类型']].drop_duplicates()) > 1:
+            # 检查该教师的所有课程是否都有其他教师教授
+            all_courses_shared = True
+            for _, course_row in teacher_courses.iterrows():
+                other_teachers = data[
+                    (data['课程名称'] == course_row['课程名称']) &
+                    (data['课程类型'] == course_row['课程类型']) &
+                    (data['任课教师'] != teacher)
+                ]
+                if len(other_teachers) == 0:
+                    all_courses_shared = False
+                    break
+            
+            if all_courses_shared:
+                # 找出记录数最多的课程
+                course_counts = teacher_courses.groupby(['课程名称', '课程类型']).size()
+                is_single_record = course_counts.max() == 1
+                max_count_course = course_counts.sample(1).index[0] if is_single_record else course_counts.idxmax()
+                
+                # 存储需要更新的教师和课程信息，同时记录是否为单条记录
+                teachers_to_update.append({
+                    '任课教师': teacher,
+                    '课程名称': max_count_course[0],
+                    '课程类型': max_count_course[1],
+                    'is_single': is_single_record
+                })
+                
+                # 存储可能需要删除的记录
+                for _, course_row in teacher_courses.iterrows():
+                    if (course_row['课程名称'], course_row['课程类型']) != max_count_course:
+                        potential_deletions.append({
+                            '任课教师': teacher,
+                            '课程名称': course_row['课程名称'],
+                            '课程类型': course_row['课程类型']
+                        })
+
+    # 验证删除操作是否全
+    safe_deletions = []
+    for deletion in potential_deletions:
+        # 检查删除后是否还有其他教师教授该课程
+        remaining_records = data[
+            (data['课程名称'] == deletion['课程名称']) &
+            (data['课程类型'] == deletion['课程类型']) &
+            (~data['任课教师'].isin([d['任课教师'] for d in potential_deletions if 
+                d['课程名称'] == deletion['课程名称'] and 
+                d['课程类型'] == deletion['课程类型']]))
+        ]
+        if len(remaining_records) > 0:
+            safe_deletions.append(deletion)
+
+    # 执行安全的删除操作
+    for deletion in safe_deletions:
+        mask = (
+            (data['任课教师'] == deletion['任课教师']) &
+            (data['课程名称'] == deletion['课程名称']) &
+            (data['课程类型'] == deletion['课程类型'])
+        )
+        data = data[~mask]
+
+    # 更新选定课程的情况说明
+    for update in teachers_to_update:
+        mask = (
+            (data['任课教师'] == update['任课教师']) &
+            (data['课程名称'] == update['课程名称']) &
+            (data['课程类型'] == update['课程类型'])
+        )
+        # 如果是单条记录，设置为"必选"，否则设置为"必须选定该老师这门课程的任选其一"
+        data.loc[mask, '情况说明'] = '必选' if update['is_single'] else '在该老师这门课程中任选其一'
+
+    # 新增处理逻辑
+    # 找出所有"任选其一"相关的记录并按课程分组
+    optional_courses = data[
+        (data['情况说明'].isin(['任选其一', '在该老师这门课程中任选其一'])) &
+        (data.groupby(['课程名称', '课程类型'])['任课教师'].transform('count') > 1)
+    ]
+    
     course_groups = optional_courses.groupby(['课程名称', '课程类型'])
     
-    # 存储需要删除的记录
-    records_to_remove = []
+    records_to_delete = []
     
-    # 处理每个课程组
     for (course_name, course_type), group in course_groups:
-        teachers = group['任课教师'].unique()
-        teachers_with_mandatory = []
+        has_teacher_specific = '在该老师这门课程中任选其一' in group['情况说明'].values
         
-        # 检查每个教师是否有必选课程
-        for teacher in teachers:
-            has_mandatory = data[
-                (data['任课教师'] == teacher) & 
-                (data['情况说明'] == '必选')
-            ].shape[0] > 0
+        if has_teacher_specific:
+            # 处理情况1：存在"在该老师这门课程中任选其一"的组别
+            optional_teachers = group[group['情况说明'] == '任选其一']['任课教师'].unique()
+            if len(optional_teachers) == 0:
+                continue
+                
+            for teacher in optional_teachers:
+                # 检查该教师的其他课程
+                other_courses = data[data['任课教师'] == teacher]
+                other_courses = other_courses[
+                    ~((other_courses['课程名称'] == course_name) & 
+                      (other_courses['课程类型'] == course_type))
+                ]
+                
+                # 检查是否有重要课程
+                has_important_course = False
+                
+                # 检查必选或特定课程
+                if other_courses['情况说明'].isin(['必选', '在该老师这门课程中任选其一']).any():
+                    has_important_course = True
+                
+                # 检查是否有独教的任选其一课程
+                for _, other_course in other_courses[other_courses['情况说明'] == '任选其一'].iterrows():
+                    course_teachers = data[
+                        (data['课程名称'] == other_course['课程名称']) &
+                        (data['课程类型'] == other_course['课程类型'])
+                    ]['任课教师'].unique()
+                    if len(course_teachers) == 1:
+                        has_important_course = True
+                        break
+                
+                if has_important_course:
+                    records_to_delete.append({
+                        '任课教师': teacher,
+                        '课程名称': course_name,
+                        '课程类型': course_type
+                    })
+        else:
+            # 处理情况2：只有"任选其一"的组别
+            potential_deletions = []
+            for _, row in group.iterrows():
+                teacher = row['任课教师']
+                # 检查该教师的其他课程
+                other_courses = data[data['任课教师'] == teacher]
+                other_courses = other_courses[
+                    ~((other_courses['课程名称'] == course_name) & 
+                      (other_courses['课程类型'] == course_type))
+                ]
+                
+                # 检查是否有重要课程
+                has_important_course = False
+                
+                # 检查必选或特定课程
+                if other_courses['情况说明'].isin(['必选', '在该老师这门课程中任选其一']).any():
+                    has_important_course = True
+                
+                # 检查是否有独教的任选其一课程
+                for _, other_course in other_courses[other_courses['情况说明'] == '任选其一'].iterrows():
+                    course_teachers = data[
+                        (data['课程名称'] == other_course['课程名称']) &
+                        (data['课程类型'] == other_course['课程类型'])
+                    ]['任课教师'].unique()
+                    if len(course_teachers) == 1:
+                        has_important_course = True
+                        break
+                
+                if has_important_course:
+                    potential_deletions.append({
+                        '任课教师': teacher,
+                        '课程名称': course_name,
+                        '课程类型': course_type
+                    })
             
-            if has_mandatory:
-                teachers_with_mandatory.append(teacher)
-        
-        # 如果不是所有教师都有必选课程，则处理删除逻辑
-        if teachers_with_mandatory and len(teachers_with_mandatory) < len(teachers):
-            for teacher in teachers_with_mandatory:
-                records_to_remove.append({
-                    '任课教师': teacher,
-                    '课程名称': course_name,
-                    '课程类型': course_type
-                })
+            # 检查删除后是否还有其他教师
+            remaining_teachers = set(group['任课教师']) - {d['任课教师'] for d in potential_deletions}
+            if remaining_teachers:  # 如果删除后还有教师剩余
+                records_to_delete.extend(potential_deletions)
     
-    # 删除标记的记录
-    if records_to_remove:
-        for record in records_to_remove:
-            mask = (
-                (data['任课教师'] == record['任课教师']) & 
-                (data['课程名称'] == record['课程名称']) & 
-                (data['课程类型'] == record['课程类型'])
-            )
-            data = data[~mask]
-    
-    # 更新剩余单条记录的状态
-    course_counts = data.groupby(['课程名称', '课程类型']).size()
-    single_courses = course_counts[course_counts == 1].index
-    
-    for course_name, course_type in single_courses:
+    # 执行删除操作
+    for deletion in records_to_delete:
         mask = (
-            (data['课程名称'] == course_name) & 
-            (data['课程类型'] == course_type)
+            (data['任课教师'] == deletion['任课教师']) &
+            (data['课程名称'] == deletion['课程名称']) &
+            (data['课程类型'] == deletion['课程类型'])
         )
-        data.loc[mask, '情况说明'] = '必选'
+        data = data[~mask]
+    
+    # 更新剩余单条记录的情况说明
+    single_records = data.groupby(['课程名称', '课程类型']).filter(lambda x: len(x) == 1)
+    data.loc[single_records.index, '情况说明'] = '必选'
+
+    data.to_excel('../test-algorithm/data_preprocess.xlsx', index=False)
     
     return data
 
@@ -167,9 +324,9 @@ def get_supervisor_course_data(supervisor_data, course_data):
     """
     # 匹配督导姓名与课程数据
     supervisor_course_data = course_data[course_data['任课教师'].isin(supervisor_data['姓名'])]
-    # 对筛选得到的数据按“任课教师”分组，并对“开课校区”和“课表信息”列进行聚合处理，在聚合过程种，“开课校区”列的值会去重并用逗号连接，“课表信息”列的值会直接用逗号连接
+    # 对筛选得到的数据按"任课教师"分组，并对"开课校区"和"课表信息"列进行聚合处理，在聚合过程种，"开课校区"列的值会去重并用逗号连接，"课表信息"列的值会直接用逗号连接
     supervisor_course_data = supervisor_course_data.groupby('任课教师').agg({'开课校区': lambda x: ','.join(set(x.dropna().astype(str))), '课表信息': lambda x: ','.join(x.dropna().astype(str))}).reset_index()
-    # 函数遍历supervisor_data中的每一行，检查每个督导是否在 supervisor_course_data中。如果某个督导不在supervisor_course_data 中，函数会为该督导添加一条空记录，记录中开课校区和课表信息列为空字符串或空字典。
+    # 函数遍历supervisor_data中的每一行，检查每个督导是否在 supervisor_course_data中。���果某个督导不在supervisor_course_data 中，函数会为该督导添加一条空记录，记录中开课校区和课表信息列为空字符串或空字典。
     for index, row in supervisor_data.iterrows():
         if row['姓名'] not in supervisor_course_data['任课教师'].values:
             new_row = {'任课教师': row['姓名'], '开课校区': '', '课表信息': '{}'}
